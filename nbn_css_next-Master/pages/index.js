7e2f109caaba7e4962ddde6449455538e4c0528e
Khushi0298 <80584462+Khushi0298@users.noreply.github.com>
2021-05-16 12:13:03 +0530

    Add files via upload

---
+++ nbn_css_next-Master/pages/index.js
@@ -0,0 +1,14 @@
+// import styles from '../styles/Home.module.css'
+// className={styles.container}
+
+import Link from 'next/link'
+
+export default function Home() {
+
+
+  return (
+    <>
+      <h2>welcome to nbn css</h2>
+    </>
+  )
+}
